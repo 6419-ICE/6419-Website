d486de324fa337ad1acf0e1af74fb9ff54e170f8
Hacker41 <199835595+Ryann-4@users.noreply.github.com>
2025-06-29 21:49:38 +0000

    added facebook icon

--- footer.js
+++ footer.js
@@ -4,7 +4,7 @@ footerLeft.innerHTML = `<p>© ICE</p>`;
 
 const footerRight = document.createElement("footer");
 footerRight.style.cssText = "background-color:transparent; text-align:right;";
-footerRight.innerHTML = `<p>socials</p>`;
+footerRight.innerHTML = `<p><a class="socials"href="https://www.facebook.com/IowaICE/"><img src="https://codehs.com/uploads/00e3f072f91daff5ee3be734d74c49b9" class="icons" alt="facebook"></a></p>`;
 
 document.body.appendChild(footerLeft);
 document.body.appendChild(footerRight);
