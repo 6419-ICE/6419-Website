1ff205c810b503389a2acc9c2ae6577f279632f6
Hacker41 <199835595+Ryann-4@users.noreply.github.com>
2025-11-11 19:52:45 -0600

    made code look pretty

--- global-header.js
+++ global-header.js
@@ -1,48 +1,66 @@
 const headerHTML = `
-<header>
-  <div style="display: flex; align-items: center;">
-    <button class="menu-button" onclick="toggleSidebar()">
-      <span></span>
-      <span></span>
-      <span></span>
-    </button>
-  </div>
-  <div style="display: flex; align-items: center; gap: 40px; margin-right: 20px;">
-    <div class="search-container">
-      <div class="search-bar">
-        <input type="text" id="searchInput" placeholder="Search">
-        <button style="font-weight:bolder; margin-left:9px;" id="searchBtn">
-        <img src="https://codehs.com/uploads/d32d2b2c0d288d661e2dc2a5aa74f91c" style="margin-left:-10px;"></button>
-        <div id="searchResults" class="search-results"></div>
-      </div>
-    </div>
-    <script src="searchbar.js">    
-    </script>
-    <a href="index">
-      <img class="logo" src="/6419-Website/res/logo.png" alt="ICE">
-    </a>
-  </div>
-</header>
-<div id="sidebar" class="sidebar">
-    <a href="https://6419-ice.github.io/6419-Website/">Home</a>
-    <hr style="border-top: 1px;margin: 0;">
-    <a href="about">About</a>
-    <hr style="border-top: 1px;margin: 0;">
-    <a href="register">Register</a>
-    <hr style="border-top: 1px;margin: 0;">
-    <a href="donate">Donate</a>
-    <hr style="border-top: 1px;margin: 0;">
-    <a href="sponsors">Sponsors</a>
-    <hr style="border-top: 1px;margin: 0;">
-    <a href="events">Events</a>
-    <hr style="border-top: 1px;margin: 0;">
-    <a href="contact">Contact Us</a>
-    <hr style="border-top: 1px;margin: 0;">
-  </div>
-  <br>
-  <br>
+	<header>
+  	<div style="display: flex; align-items: center;">
+  	  <button class="menu-button" onclick="toggleSidebar()">
+  	    <span></span>
+  	    <span></span>
+  	    <span></span>
+  	  </button>
+  	</div>
+  	<div style="display: flex; align-items: center; gap: 40px; margin-right: 20px;">
+  	  <div class="search-container">
+  	    <div class="search-bar">
+  	      <input type="text" id="searchInput" placeholder="Search">
+  	      <button style="font-weight:bolder; margin-left:9px;" id="searchBtn">
+  	      	<img src="https://codehs.com/uploads/d32d2b2c0d288d661e2dc2a5aa74f91c" style="margin-left:-10px;">
+					</button>
+  	      <div id="searchResults" class="search-results"></div>
+  	    </div>
+    	</div>
+    	<script src="searchbar.js">    
+    	</script>
+    	<a href="index">
+      	<img class="logo" src="/6419-Website/res/logo.png" alt="ICE">
+    	</a>
+  	</div>
+	</header>
+	<div id="sidebar" class="sidebar">
+   		<a href="https://6419-ice.github.io/6419-Website/">
+				Home
+			</a>
+    	<hr style="border-top: 1px;margin: 0;">
+    	<a href="about">
+				About
+			</a>
+    	<hr style="border-top: 1px;margin: 0;">
+    	<a href="register">
+				Register
+			</a>
+    	<hr style="border-top: 1px;margin: 0;">
+    	<a href="donate">
+				Donate
+			</a>
+    	<hr style="border-top: 1px;margin: 0;">
+    	<a href="sponsors">
+				Sponsors
+			</a>
+    	<hr style="border-top: 1px;margin: 0;">
+    	<a href="events">
+				Events
+			</a>
+    	<hr style="border-top: 1px;margin: 0;">
+    	<a href="contact">
+				Contact Us
+			</a>
+    	<hr style="border-top: 1px;margin: 0;">
+  	</div>
+  	<br>
+  	<br>
 `;
-
+function toggleSidebar() {
+  const sidebar = document.getElementById("sidebar");
+  sidebar.classList.toggle("open");
+}
 document.addEventListener("DOMContentLoaded", () => {
   const headerWrapper = document.createElement("div");
   headerWrapper.innerHTML = headerHTML;
