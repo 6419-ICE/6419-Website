90e030e753c545d2cadb875f02ccd623ef8ba330
Hacker41 <199835595+Ryann-4@users.noreply.github.com>
2025-12-09 20:40:56 -0600

    added stuff

--- frame.js
+++ frame.js
@@ -55,9 +55,15 @@ const headerHTML = `
 			Contact Us
 		</a>
     	<hr style="border-top: 1px;margin: 0;">
+		<a href="members.html">
+			Member Resources
+		</a>
+		<hr style="border-top: 1px;margin: 0;">
   	</div>
   	<br>
   	<br>
+	<br>
+	<br>
 `;
 function toggleSidebar() {
   const sidebar = document.getElementById("sidebar");
@@ -71,19 +77,19 @@ const footerRight = document.createElement("footer");
 footerRight.style.cssText = "background-color:transparent; text-align:right; z-index:999;";
 footerRight.innerHTML = `
 	<p>
-		<a href="https://www.facebook.com/IowaICE/">
+		<a style="text-decoration:none;" href="https://www.facebook.com/wmdfrc6419">
 			<img src="https://codehs.com/uploads/00e3f072f91daff5ee3be734d74c49b9" class="icons" alt="facebook">
 		</a>
-		<a href="https://www.youtube.com/channel/UCLY9sPpdscFBkIIbrT1DKiQ">
+		<a style="text-decoration:none;" href="https://www.youtube.com/channel/UCLY9sPpdscFBkIIbrT1DKiQ">
 			<img src="https://codehs.com/uploads/948d8323a9b44f3b408e5eaf32935f81" class="icons" alt="youtube">
 		</a>
-		<a href="https://twitter.com/iowaice?lang=en">
+		<a style="text-decoration:none;" href="https://twitter.com/iowaice?lang=en">
 			<img src="https://codehs.com/uploads/1f4154cde184ea0aab9ea5bf7048b3d7" class="icons" alt="X">
 		</a>
-		<a href="https://www.instagram.com/iowaice6419">
+		<a style="text-decoration:none;" href="https://www.instagram.com/iowaice6419">
 			<img src="https://codehs.com/uploads/6661776061c91e21bce06d4c881e8bb8" class="icons" alt="Instagram">
 		</a>
-		<a href="https://github.com/6419-ICE">
+		<a style="text-decoration:none;" href="https://github.com/6419-ICE">
 			<img src="https://codehs.com/uploads/47c64d37b96e7570b39060260d1002b6" class="icons" alt="Github">
 		</a>
 	</p>`;
